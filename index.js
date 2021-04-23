b38ab4b0eac791ac6bb150e1acafa7e12f735703
wang.xander <wang.xander@gmail.com>
2021-04-23 12:19:24 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,108 @@
+// database
+// const main = require("./localDB/index").main;
+// const insertDestination = require("./localDB/index").insertDestination;
+// const clientDB = require("./localDB/index").clientDB;
+const {
+  main,
+  insertDestination,
+  clientDB,
+  deleteAllDestination,
+  deleteDestinationByUid,
+} = require("./localDB/index");
+const cors = require("cors");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+
+//require express
+const express = require("express");
+const server = express();
+server.use("/", cors());
+server.use(bodyParser.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3000; // 3000 or server system defined
+
+server.listen(PORT, () => {
+  console.log(`Listening Port...${PORT}`);
+});
+
+main();
+// CRUD
+// functions:   create  read    update  delete
+// methods:     post    get     put     delete
+
+// POST : sending post request to server
+// expects {name, location, description} from user
+
+// dbClient.connect(async (err) => {
+//   const collection = dbClient.db("destination").collection("devices");
+//   // perform actions on the collection object
+//   console.log("Connected to MongoDB");
+//   //console.log(collection);
+
+//   // GET / => READ
+server.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+//   // POST
+server.post("/postDesitination", (req, res) => {
+  console.log(req.body);
+  // TODO: 
+  insertDestination(
+    clientDB,
+    createDestination(
+      ,
+      "seattle",
+      "where i live",
+      "photo url",
+      "space needle"
+    )
+  );
+  res.send("inserted");
+  //   db.main(server).then((x) => {
+  //     res.redirect("/");
+  //   });
+});
+
+// PUT
+server.put("/putDestination", (req, res) => {
+  //insertDestination(clientDB, localDB[0]);
+  console.log(req.query);
+  console.log(req.query.uid);
+  console.log(req.query.description);
+  console.log(req.body);
+  //   localDB.push(
+  //     createDestination(req.query.uid, "", req.query.description, "", "", "")
+  //   );
+  res.send(localDB);
+});
+
+server.delete("/deleteAllDestination", (req, res) => {
+  deleteAllDestination(clientDB);
+  res.send("deleted all");
+});
+
+server.delete("/deleteDestinationByUid", (req, res) => {
+  deleteDestinationByUid(clientDB, req.query.uid);
+  res.send("delete uid");
+});
+
+function createDestination(uid, location, description, photo, name) {
+  return { uid, location, description, photo, name };
+}
+
+const localDB = [
+  {
+    uid: 23456,
+    name: "Efiile tower",
+    location: "Paris",
+    photo: "url",
+    description: "Romantic place",
+  },
+  {
+    uid: 34567,
+    name: "The needle",
+    location: "Seattle",
+    photo: "url",
+    description: "Another Romantic place",
+  },
+];
---
+++ localDB/index.js
@@ -0,0 +1,74 @@
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://apiserver:dmME9dDnXHZpCJX@cluster0.18y6w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const clientDB = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function main() {
+  try {
+    await clientDB.connect();
+    await listDB(clientDB);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//main().catch(console.error);
+
+async function listDB(clientDB) {
+  const dblist = await clientDB.db().admin().listDatabases();
+  console.log("Databases:");
+  dblist.databases.forEach((db) => console.log(`- ${db.name}`));
+}
+
+async function insertDestination(clientDB, newListing) {
+  const result = await clientDB
+    .db("destinations")
+    .collection("listOfDestinations")
+    .insertOne(newListing);
+  console.log(`New destination was created with id:${result.insertedId}`);
+}
+
+async function deleteAllDestination(clientDB) {
+  const result = await clientDB
+    .db("destinations")
+    .collection("listOfDestinations");
+  if (result !== undefined) {
+    result.drop();
+  }
+  console.log("Destination List Removed");
+}
+
+async function deleteDestinationByUid(clientDB, uid) {
+  const result = await clientDB
+    .db("destinations")
+    .collection("listOfDestinations")
+    .deleteOne({ uid: uid });
+  console.log(`Destination with UID ${uid} was removed ${result}`);
+}
+
+// const db = clientDB.connect((err) => {
+//   const collection = clientDB.db("test").collection("devices");
+//   // perform actions on the collection object
+//   console.log("Connected to MongoDB");
+//   console.log(collection);
+//   collection
+//     .insertOne({ name: "first", location: "seattle" })
+//     .then((result) => {
+//       console.log(result);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+//   clientDB.close();
+// });
+
+module.exports = {
+  clientDB,
+  main,
+  insertDestination,
+  deleteAllDestination,
+  deleteDestinationByUid,
+};
