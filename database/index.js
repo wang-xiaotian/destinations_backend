172b662beb15911ecc2d9672921ccaddb339d239
wang.xander <wang.xander@gmail.com>
2021-04-25 19:48:35 -0700

    updated database folder

---
+++ database/index.js
@@ -0,0 +1,64 @@
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://apiserver:dmME9dDnXHZpCJX@cluster0.18y6w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const clientDB = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const databaseName = "destinations";
+const collectionName = "listOfDestinations";
+
+// connect to database
+async function main() {
+  try {
+    await clientDB.connect();
+    await listDB(clientDB);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function listDB(clientDB) {
+  const dblist = await clientDB.db().admin().listDatabases();
+  console.log("Databases:");
+  dblist.databases.forEach((db) => console.log(`- ${db.name}`));
+}
+
+// insert one destination in database
+async function insertDestination(clientDB, newListing) {
+  const result = await clientDB
+    .db(databaseName)
+    .collection(collectionName)
+    .insertOne(newListing);
+  console.log(`New destination was created with id:${result.insertedId}`);
+}
+
+// return the list of all destinations
+async function getAllDestinations(clientDB) {
+  return await clientDB.db(databaseName).collection(collectionName);
+}
+
+// delete all destinations from database
+// this verson only support one database
+async function deleteAllDestination(clientDB) {
+  const result = await clientDB.db(databaseName).collection(collectionName);
+  if (result !== undefined) {
+    result.drop();
+  }
+  console.log("Destination List Removed");
+}
+
+async function deleteDestinationByUid(clientDB, uid) {
+  const result = await clientDB
+    .db(databaseName)
+    .collection(collectionName)
+    .deleteOne({ uid: uid });
+  console.log(`Destination with UID ${uid} was removed ${result}`);
+}
+module.exports = {
+  clientDB,
+  main,
+  insertDestination,
+  deleteAllDestination,
+  deleteDestinationByUid,
+};
